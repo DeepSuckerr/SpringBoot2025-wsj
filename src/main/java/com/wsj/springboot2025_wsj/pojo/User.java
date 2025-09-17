5d6bae423ea386e7bf385aec2d1244ef218e80da
DeepSuckerr <whiskeyaka9@gmail.com>
2025-09-17 17:36:25 +0800

    更新代码

---
+++ src/main/java/com/wsj/springboot2025_wsj/pojo/User.java
@@ -0,0 +1,14 @@
+package com.wsj.springboot2025_wsj.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private Integer id;
+    private String userName;
+    private String password;
+    private String age;
+    private String tel;
+    private String idCard;
+
+}
