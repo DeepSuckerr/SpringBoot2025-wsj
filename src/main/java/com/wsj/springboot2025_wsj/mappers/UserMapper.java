464bb7fed69c7f432c6d4c9118732ab12acd67cb
DeepSuckerr <whiskeyaka9@gmail.com>
2025-09-17 17:50:06 +0800

    很好

---
+++ src/main/java/com/wsj/springboot2025_wsj/mappers/UserMapper.java
@@ -0,0 +1,10 @@
+package com.wsj.springboot2025_wsj.mappers;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wsj.springboot2025_wsj.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+
+}
--- src/main/java/com/wsj/springboot2025_wsj/pojo/User.java
+++ src/main/java/com/wsj/springboot2025_wsj/pojo/User.java
@@ -1,8 +1,16 @@
 package com.wsj.springboot2025_wsj.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 import java.io.Serializable;
 
+
+@Data
+@TableName("tb_user")
 public class User implements Serializable {
+
 
     private Integer id;
     private String userName;
